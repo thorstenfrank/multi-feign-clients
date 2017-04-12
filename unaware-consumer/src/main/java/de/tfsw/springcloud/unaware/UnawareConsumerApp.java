8a2951712082afb8f50a4cc7a1c8a81001a92ed2
Thorsten Frank <thorsten@tfsw.de>
2017-04-12 10:11:25 +0200

    Add app without direct dependencies to provider

--- unaware-consumer/src/main/java/de/tfsw/springcloud/unaware/UnawareConsumerApp.java
+++ unaware-consumer/src/main/java/de/tfsw/springcloud/unaware/UnawareConsumerApp.java
@@ -1,20 +1,14 @@
-package de.tfsw.cloud.consumer;
+package de.tfsw.springcloud.unaware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-/**
- * Hello world!
- *
- */
 @EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
-public class ServiceConsumerApp {
+public class UnawareConsumerApp {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceConsumerApp.class, args);
+		SpringApplication.run(UnawareConsumerApp.class, args);
 	}
 }
